6c47898aa0e0b4db899765872c4254a9c419217f
Steveland Daniels <steveland@backbase.com>
2018-10-01 19:59:27 +0100

    Changed a word

---
+++ Exercise_2-HelloWorld/helloWorld.py
@@ -0,0 +1,9 @@
+"""
+Exercise 2 - Create a program that prints out the following sentences:
+
+Hello world!
+(And all the people of the world)
+"""
+
+print("Hello world!")
+print("(And all the people of the world)")
